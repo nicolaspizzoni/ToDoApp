c86b599d1be9796088ae230ed202490f99ee63dc
nicozzi <nickpizzoni@outlook.com>
2021-05-21 19:57:18 -0300

    Desafio concluído

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import { Alert } from 'react-native';
 
 import { Header } from '../components/Header';
 import { MyTasksList } from '../components/MyTasksList';
@@ -11,18 +12,33 @@ interface Task {
 }
 
 export function Home() {
-  // const [tasks, setTasks] = useState<Task[]>([]);
+  const [tasks, setTasks] = useState<Task[]>([]);
 
   function handleAddTask(newTaskTitle: string) {
-    //TODO - add new task if it's not empty
+    if(newTaskTitle === ''){
+      Alert.alert("Atenção", "Favor informar uma tarefa para adicionar")
+      return;
+    }
+    setTasks(oldState => [...oldState, {
+      id: new Date().getTime(),
+      title: newTaskTitle,
+      done: false
+    }]);
   }
 
   function handleMarkTaskAsDone(id: number) {
-    //TODO - mark task as done if exists
+    const taskHasDone = tasks.map(t => {
+      if(t.id === id) {
+        t.done = !t.done
+      }
+      return t;
+    })
+    
+    setTasks(oldState => [...taskHasDone]);
   }
 
   function handleRemoveTask(id: number) {
-    //TODO - remove task from state
+    setTasks(tasks.filter(t => t.id !== id))
   }
 
   return (
